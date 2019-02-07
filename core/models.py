87eb2a01e6e605c10b84d7a3a8a338ddaca3ce45
marush-in <shin.marumoto@gmail.com>
2019-02-07 17:19:10 +0900

    Added code models.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    PP = 'PP'
+    ID = 'ID'
+    OT = 'OT'
+
+    DOC_TYPES = (
+        (PP, 'PassPort'),
+        (ID, 'Identity card'),
+        (OT, 'Others'),
+    )
+
+    dtype = models.CharField(choices=DOC_TYPES, max_length=2)
+    doc_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.doc_number
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    documents = models.ManyToManyField(Document)
+
+    def __str__(self):
+        return self.name
