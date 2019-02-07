66485a2b695014d19f3efa110ddc0588b303a0e4
marush-in <shin.marumoto@gmail.com>
2019-02-07 18:37:57 +0900

    Setup of the Django Rest Framework and creating the first API Endpoint.

--- core/models.py
+++ core/models.py
@@ -19,7 +19,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     professions = models.ManyToManyField(Profession)
-    deta_sheet = models.OneToOneField(DataSheet, on_delete=models.CASCADE , null=True, blank=True)
+    data_sheet = models.OneToOneField(DataSheet, on_delete=models.CASCADE , null=True, blank=True)
 
     def __str__(self):
         return self.name
