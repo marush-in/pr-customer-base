66d2eeb340d56de8fe748b2debf3e2566f60725b
marush-in <shin.marumoto@gmail.com>
2019-02-07 17:44:33 +0900

    Added extra database relationships.

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Document, Customer
+from .models import Document, Customer, Profession, DataSheet
 
 
 admin.site.register(Document)
 admin.site.register(Customer)
+admin.site.register(Profession)
+admin.site.register(DataSheet)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 
 
+class Profession(models.Model):
+    description = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.description
+
+class DataSheet(models.Model):
+    description = models.CharField(max_length=50)
+    historical_data = models.TextField()
+
+    def __str__(self):
+        return self.description
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    professions = models.ManyToManyField(Profession)
+    deta_sheet = models.OneToOneField(DataSheet, on_delete=models.CASCADE , null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Document(models.Model):
     PP = 'PP'
     ID = 'ID'
@@ -14,15 +38,7 @@ class Document(models.Model):
 
     dtype = models.CharField(choices=DOC_TYPES, max_length=2)
     doc_number = models.CharField(max_length=50)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.doc_number
-
-
-class Customer(models.Model):
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    documents = models.ManyToManyField(Document)
-
-    def __str__(self):
-        return self.name
