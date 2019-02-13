6070b6100dc682e7f9c4960b8679db20730f16cc
marush-in <shin.marumoto@gmail.com>
2019-02-13 15:27:50 +0900

    Setup the project for Heroku part 2.

--- config/settings.py
+++ config/settings.py
@@ -109,6 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # REST_FRAMEWORK
 REST_FRAMEWORK = {
